a61f593dbcec7c42c6662ef5b8170fd3d56d0791
Felipe Brunelli de Andrade <fbruandrade@gmail.com>
2021-01-10 01:29:08 -0300

    first commit

---
+++ src/main/java/dev/fandrade/myoperator/QuarkusOperator.java
@@ -0,0 +1,47 @@
+package dev.fandrade.myoperator;
+
+import io.fabric8.kubernetes.client.KubernetesClient;
+import io.javaoperatorsdk.operator.Operator;
+import io.javaoperatorsdk.operator.api.config.ConfigurationService;
+import io.quarkus.runtime.Quarkus;
+import io.quarkus.runtime.QuarkusApplication;
+import io.quarkus.runtime.annotations.QuarkusMain;
+
+import javax.inject.Inject;
+
+@QuarkusMain
+public class QuarkusOperator implements QuarkusApplication {
+
+    @Inject
+    KubernetesClient client;
+
+    @Inject
+    Operator operator;
+
+    @Inject
+    ConfigurationService configuration;
+
+    @Inject
+    CustomServiceController controller;
+
+    private boolean _running = false;
+
+    public static void main(String... args) {
+        Quarkus.run(QuarkusOperator.class, args);
+    }
+
+    @Override
+    public int run(String... args) throws Exception {
+        final var config = configuration.getConfigurationFor(controller);
+        System.out.println("CR class: " + config.getCustomResourceClass());
+        System.out.println("Doneable class = " + config.getDoneableClass());
+        _running = true;
+        Quarkus.waitForExit();
+        _running = false;
+        return 0;
+    }
+
+    public boolean isRunning() {
+        return _running;
+    }
+}
---
+++ src/test/java/dev/fandrade/myoperator/NativeoperatorResourceIT.java
@@ -0,0 +1,9 @@
+package dev.fandrade.myoperator;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeoperatorResourceIT extends operatorResourceTest {
+
+    // Execute the same tests but in native mode.
+}
