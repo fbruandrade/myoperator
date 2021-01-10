1c160414ce3e9b0ca50b6c1199a7ec1daa55ac05
Felipe Brunelli de Andrade <fbruandrade@gmail.com>
2021-01-10 01:37:37 -0300

    first commit

--- src/test/java/dev/fandrade/myoperator/NativeoperatorResourceIT.java
+++
@@ -1,9 +0,0 @@
-package dev.fandrade.myoperator;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeoperatorResourceIT extends operatorResourceTest {
-
-    // Execute the same tests but in native mode.
-}
